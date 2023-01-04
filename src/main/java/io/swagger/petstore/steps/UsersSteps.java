52a0da6cbafef1a12816a96cca1962ceb966ec80
pooja ronak patel <pjmaradiya@gmail.com>
2023-01-04 16:35:53 +0000

    petstore-project4-serenity-WEEK18-POOJA

---
+++ src/main/java/io/swagger/petstore/steps/UsersSteps.java
@@ -0,0 +1,80 @@
+package io.swagger.petstore.steps;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.swagger.petstore.constants.Endpoints;
+import io.swagger.petstore.model.UsersPojo;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+import java.util.HashMap;
+
+public class UsersSteps {
+
+    @Step("Create user with userName : {1}, firstName:{2}, lastName: {3},email:{4}")
+    public ValidatableResponse createNewUser(int id, String userName, String firstName, String lastName, String email, String password, String phone, String userStatus){
+
+        UsersPojo usersPojo = new UsersPojo();
+        usersPojo.setId(id);
+        usersPojo.setUsername(userName);
+        usersPojo.setFirstname(firstName);
+        usersPojo.setLastname(lastName);
+        usersPojo.setEmail(email);
+        usersPojo.setPassword(password);
+        usersPojo.setPhone(phone);
+        usersPojo.setUserStatus(userStatus);
+
+        return SerenityRest.given().log().all()
+                .header("Content-Type", "application/json")
+                .body(usersPojo)
+                .when()
+                .post(Endpoints.CREATE_USER)
+                .then();
+    }
+
+    @Step("Getting the user information with firstName: {0}")
+    public HashMap<String, Object> getUserByUserName(String username) {
+//        String p1 = "findAll{it.username == '";
+//        String p2 = "'}.get(0)";
+        return SerenityRest.given().log().all()
+                .pathParam("username", username)
+                .when()
+                .get(Endpoints.GET_USER_BY_USERNAME)
+                .then()
+                .statusCode(200)
+                .extract().path("");
+    }
+
+    @Step("Updating User information with userName: {1}, firstName: {2}, lastName: {3}, email: {4}, programme: {5} and courses: {6}")
+
+    public ValidatableResponse updateUser(int id, String username, String firstName, String lastName, String email, String password, String phone, String userStatus) {
+        UsersPojo usersPojo = new UsersPojo();
+        usersPojo.setId(id);
+        usersPojo.setUsername(username);
+        usersPojo.setFirstname(firstName);
+        usersPojo.setLastname(lastName);
+        usersPojo.setEmail(email);
+        usersPojo.setPassword(password);
+        usersPojo.setPhone(phone);
+        usersPojo.setUserStatus(userStatus);
+
+        return  SerenityRest.given().log().all()
+                .contentType(ContentType.JSON)
+                .pathParam("username", username)
+                .when()
+                .body(usersPojo)
+                .put(Endpoints.UPDATE_USER_BY_USERNAME)
+                .then();
+
+    }
+
+    @Step
+    public ValidatableResponse deleteUser(String username) {
+
+        return SerenityRest.given().log().all()
+                .pathParam("username", username)
+                .when()
+                .delete(Endpoints.DELETE_USER_BY_USERNAME)
+                .then();
+    }
+}
---
+++ src/test/java/io/swagger/petstore/crudtests/UsersCRUDTest.java
@@ -0,0 +1,63 @@
+package io.swagger.petstore.crudtests;
+
+import io.restassured.response.ValidatableResponse;
+import io.swagger.petstore.steps.UsersSteps;
+import io.swagger.petstore.testbase.TestBase;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.hasValue;
+
+@RunWith(SerenityRunner.class)
+public class UsersCRUDTest extends TestBase {
+
+    static int id = 15;
+    static String userName = "ppoja435";
+    static String firstName = "smith";
+    static String lastName = "bull";
+    static String email = "auto200@gmail.com";
+    static String password = "test123";
+    static String phone = "9876543212";
+    static String userStatus = "1";
+
+
+    @Steps
+    UsersSteps usersSteps;
+
+    @Title("This test will Create a new User")
+    @Test
+    public void test001(){
+        ValidatableResponse response = usersSteps.createNewUser(id, userName,firstName,lastName,email,password,phone,userStatus);
+        response.log().all().statusCode(200);
+    }
+
+    @Title("Verify user is added")
+    @Test
+    public void test002(){
+        HashMap<String, Object> userMap = usersSteps.getUserByUserName(userName);
+        Assert.assertThat(userMap, hasValue(userName));
+    }
+
+    @Title("Update the user information and verify the updated information")
+    @Test
+    public void test003() {
+        userName = userName + "_updated";
+        usersSteps.updateUser(id,userName,firstName,lastName,email,password,phone,userStatus);
+        HashMap<String, Object> userMap = usersSteps.getUserByUserName(userName);
+        Assert.assertThat(userMap, anything(firstName));
+    }
+
+    @Title("Verify user was deleted")
+    @Test
+    public void test004() {
+        usersSteps.deleteUser(userName).statusCode(200).log().all();
+
+    }
+}
